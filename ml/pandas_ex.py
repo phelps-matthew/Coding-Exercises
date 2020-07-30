bf31eb84ba3998647d1f1f14b107f865ba37017a
phelps-matthew <mgp333@gmail.com>
2020-07-30 18:49:03 -0400

    update ipynb notes

--- ml/pandas_ex.py
+++ ml/pandas_ex.py
@@ -3,9 +3,6 @@ import pandas as pd
 fruits = pd.DataFrame(data=[[30, 21]], columns=["Apples", "Bananas"])
 d = {"a": [1, 34], "n": [24, 53], "b": [43, 32]}
 df2 = pd.DataFrame(d)
-# fmt: off
-import ipdb,os; ipdb.set_trace(context=5)  # noqa
-# fmt: on
 print(df2)
 ds = pd.Series(d)
 print(ds)
